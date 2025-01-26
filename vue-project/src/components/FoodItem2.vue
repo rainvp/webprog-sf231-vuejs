b3f255aca8a99a40061fbaa2011ca0ea4e0e5c45
Rain Cel-Anne Sophia V. Presa <rvpresa@student.apc.edu.ph>
2025-01-26 14:08:47 +0800

    added food item 2

---
+++ vue-project/src/components/FoodItem2.vue
@@ -0,0 +1,34 @@
+<template>
+    <div v-on:click="countClicks">
+      <h2>{{ name }}</h2>  
+      <p>{{ message }}</p>
+      <p id="red">You have clicked me {{ clicks }} times.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Mangoes',
+        message: 'I like mangoes',
+        clicks: 0
+      }
+    },
+    methods: {
+      countClicks() {
+        this.clicks++;
+      }
+    }
+  }
+  </script>
+  
+  <style>
+    #app > div {
+          border: dashed black 1px;
+          display: inline-block;
+          margin: 10px;
+          padding: 10px;
+          background-color: lightyellow;
+      }
+  </style> 
