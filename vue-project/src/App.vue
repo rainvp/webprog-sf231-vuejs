b2c216533d3558a5bbfc3acf7aaaef5cca9c8d9c
Rain Cel-Anne Sophia V. Presa <rvpresa@student.apc.edu.ph>
2025-01-26 13:58:15 +0800

    adding component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,18 @@
-<script></script>
- 
 <template>
-
-<h1> Hello, WEBPROG SF231! </h1>
-
-</template>
- 
-<style></style>
+    <h1>Food</h1>
+    <food-item/>
+    <food-item/>
+    <food-item/>
+  </template>
+  
+  <script></script>
+  
+  <style>
+    #app > div {
+          border: dashed black 1px;
+          display: inline-block;
+          margin: 10px;
+          padding: 10px;
+          background-color: lightgreen;
+      }
+  </style> 
