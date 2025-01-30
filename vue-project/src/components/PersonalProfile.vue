818c5967eb869c893ccde701d3311e277e9fa8f8
Rain Cel-Anne Sophia V. Presa <rvpresa@student.apc.edu.ph>
2025-01-30 14:24:30 +0800

    attempt

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -2,7 +2,10 @@
     <div>
       <nav class="navbar">
         <div class="logo">
-          <img src="https://scontent.fmnl33-1.fna.fbcdn.net/v/t1.15752-9/474667107_1150394953328908_453092773794161443_n.png?_nc_cat=100&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeFtTV2Y3K0jKg0_2qV1Ho2xsF94E5Xqu6ewX3gTleq7pyQODnFyHMmvOYxLBKQlWA6rilKeyHQG2zaatLODHo2A&_nc_ohc=rpQ5djOAujEQ7kNvgHIFAZB&_nc_zt=23&_nc_ht=scontent.fmnl33-1.fna&oh=03_Q7cD1gF1ICi8zBFjshzJ24XNdz3oZT7SBQdxXjfFdQZv9Wfp3w&oe=67BFD3DF" alt="Rain's Cafe Logo">
+          <img 
+            src="https://scontent.fmnl33-1.fna.fbcdn.net/v/t1.15752-9/474667107_1150394953328908_453092773794161443_n.png?_nc_cat=100&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeFtTV2Y3K0jKg0_2qV1Ho2xsF94E5Xqu6ewX3gTleq7pyQODnFyHMmvOYxLBKQlWA6rilKeyHQG2zaatLODHo2A&_nc_ohc=rpQ5djOAujEQ7kNvgHIFAZB&_nc_zt=23&_nc_ht=scontent.fmnl33-1.fna&oh=03_Q7cD1gF1ICi8zBFjshzJ24XNdz3oZT7SBQdxXjfFdQZv9Wfp3w&oe=67BFD3DF" 
+            alt="Rain's Cafe Logo"
+          >
         </div>
         <div class="nav-links">
           <a href="#home"><b>Home</b></a>
@@ -35,48 +38,50 @@
           </div>
         </div>
       </nav>
-
-<!-- Banner Section -->
-<header id="banner">
-  <div id="banner-content" class="row clearfix">
-      <div class="col-38">
-          <div class="section-heading">
+  
+      <!-- Banner Section -->
+      <header id="banner">
+        <div id="banner-content" class="row clearfix">
+          <div class="col-38">
+            <div class="section-heading">
               <h1>Rain's Digital Café</h1>
+            </div>
+            <a href="#" class="button">GET TO KNOW ME!</a>
           </div>
-          <a href="#" class="button">GET TO KNOW ME!</a>
+        </div>
+      </header>
+  
+      <!-- About The Page Section -->
+      <div class="w3-container" id="about">
+        <div class="w3-content" style="max-width: 1000px">
+          <h5 class="tag">ABOUT THE CAFE</h5>
+          <p>Welcome to my personal profile webpage!</p>
+          <p>
+            This is my very first step into the world of web development. 
+            I created this page to explore and learn the basics of building a website 
+            while expressing my style through a cozy café-inspired theme. 
+            It’s a small but meaningful project that represents the beginning of 
+            my journey in web design and development.
+          </p>
+        </div>
       </div>
-  </div><!--End of Row-->
-</header>
   
-<!-- About The PAGE Container -->
-<div class="w3-container" id="about">
-  <div class="w3-content" style="max-width: 1000px">
-    <h5 class="tag">ABOUT THE CAFE </h5>
-  
-    <p>Welcome to my personal profile webpage!</p>
-    <p>This is my very first step into the world of web development. 
-    I created this page to explore and learn the basics of building a website 
-    while expressing my style through a cozy café-inspired theme. 
-    It’s a small but meaningful project that represents the beginning of 
-    my journey in web design and development</p>
-
- </div>
-</div>
- 
- <!-- About ME Container -->
-<div class="w3-container" id="about">
-  <div class="w3-content" style="max-width: 1000px">
-    <h5 class="tag">ABOUT THE OWNER </h5>
-  </div>   
-    <div class="w3-panel w3-leftbar w3-light-beige">
-      <p><i>"The sun will rise and we will try again" *quote creds/smthn short i will say abt it*.</i></p>
-      <p>Chef, Coffeeist and Owner: Rain Cel-Anne Sophia V. Presa</p> 
+      <!-- About The Owner Section -->
+      <div class="w3-container" id="owner">
+        <div class="w3-content" style="max-width: 1000px">
+          <h5 class="tag">ABOUT THE OWNER</h5>
+          <div class="w3-panel w3-leftbar w3-light-beige">
+            <p><i>"The sun will rise and we will try again" *quote creds/smthn short i will say abt it*.</i></p>
+            <p>Chef, Coffeeist, and Owner: Rain Cel-Anne Sophia V. Presa</p> 
+          </div>
+        </div>
+      </div>
     </div>
-</div>  
   </template>
 
-<script>
-</script>
+<script></script>
   
-  <style>
-  </style>
+<style>
+
+</style> 
+  
