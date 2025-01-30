d0cef1321f256529457bd76e26ff3dfab63e9d4f
Rain Cel-Anne Sophia V. Presa <rvpresa@student.apc.edu.ph>
2025-01-30 11:32:50 +0800

    test

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -5,36 +5,30 @@
         <div class="logo">
           <img src="https://scontent.fmnl33-1.fna.fbcdn.net/v/t1.15752-9/474667107_1150394953328908_453092773794161443_n.png?_nc_cat=100&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeFtTV2Y3K0jKg0_2qV1Ho2xsF94E5Xqu6ewX3gTleq7pyQODnFyHMmvOYxLBKQlWA6rilKeyHQG2zaatLODHo2A&_nc_ohc=rpQ5djOAujEQ7kNvgHIFAZB&_nc_zt=23&_nc_ht=scontent.fmnl33-1.fna&oh=03_Q7cD1gF1ICi8zBFjshzJ24XNdz3oZT7SBQdxXjfFdQZv9Wfp3w&oe=67BFD3DF" alt="Rain's Cafe Logo">
         </div>
-        <div>
-          <a href="#home"><b>Home</b></a>
-          <a href="#about">About</a>
-          <a href="#gallery">Gallery</a>
-          <a href="#contact">Contact</a>
-          <div class="dropdown">
-            <button class="dropbtn">
-              <span class="dropdown-icon">&#x25BC;</span> <!-- Dropdown icon -->
-            </button>
-            <div class="dropdown-content">
-              <div class="dropdown-section">
-                <h4 class="dropdown-title">Menu</h4>
-                <a href="#menu1">Coffees</a>
-                <a href="#menu2">Pastries</a>
-                <a href="#menu3">Specials</a>
-              </div>
-              <div class="dropdown-section">
-                <h4 class="dropdown-title">Contact Us</h4>
-                <a href="#location">Our Location</a>
-                <a href="#support">Customer Support</a>
-              </div>
-              <div class="dropdown-section">
-                <h4 class="dropdown-title">Social</h4>
-                <a href="#facebook">Facebook</a>
-                <a href="#instagram">Instagram</a>
-                <a href="#twitter">Twitter</a>
-            </div>
-        </div>
+        <div class="dropdown">
+  <button class="dropbtn">
+    <span class="dropdown-icon">&#x25BC;</span> <!-- Dropdown icon -->
+  </button>
+  <div class="dropdown-content">
+    <div class="dropdown-section">
+      <h4 class="dropdown-title">Menu</h4>
+      <a href="#menu1">Coffees</a>
+      <a href="#menu2">Pastries</a>
+      <a href="#menu3">Specials</a>
     </div>
-</div>
+    <div class="dropdown-section">
+      <h4 class="dropdown-title">Contact Us</h4>
+      <a href="#location">Our Location</a>
+      <a href="#support">Customer Support</a>
+    </div>
+    <div class="dropdown-section">
+      <h4 class="dropdown-title">Social</h4>
+      <a href="#facebook">Facebook</a>
+      <a href="#instagram">Instagram</a>
+      <a href="#twitter">Twitter</a>
+    </div> 
+  </div> 
+</div> 
 </nav> 
   
       <!-- Banner Section -->
