45fb7fe58ac1e9d1c3ae605e0d821389f8ec30ef
Rain Cel-Anne Sophia V. Presa <rvpresa@student.apc.edu.ph>
2025-01-30 12:31:21 +0800

    template personal profile

---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,93 @@
+<template>
+    <div>
+      <!-- Navigation -->
+      <nav class="navbar">
+        <div class="logo">
+          <img :src="logoSrc" alt="Rain's Cafe Logo" />
+        </div>
+        <div>
+          <router-link to="#home"><b>Home</b></router-link>
+          <router-link to="#about">About</router-link>
+          <router-link to="#gallery">Gallery</router-link>
+          <router-link to="#contact">Contact</router-link>
+          <!-- Dropdown menu beside Contact -->
+          <div class="dropdown">
+            <button class="dropbtn">
+              <span class="dropdown-icon">&#x25BC;</span>
+            </button>
+            <div class="dropdown-content">
+              <div class="dropdown-section">
+                <h4 class="dropdown-title">Menu</h4>
+                <router-link to="#menu1">Coffees</router-link>
+                <router-link to="#menu2">Pastries</router-link>
+                <router-link to="#menu3">Specials</router-link>
+              </div>
+              <div class="dropdown-section">
+                <h4 class="dropdown-title">Contact Us</h4>
+                <router-link to="#location">Our Location</router-link>
+                <router-link to="#support">Customer Support</router-link>
+              </div>
+              <div class="dropdown-section">
+                <h4 class="dropdown-title">Social</h4>
+                <router-link to="#facebook">Facebook</router-link>
+                <router-link to="#instagram">Instagram</router-link>
+                <router-link to="#twitter">Twitter</router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+  
+      <!-- Banner Section -->
+      <header id="banner">
+        <div id="banner-content" class="row clearfix">
+          <div class="col-38">
+            <div class="section-heading">
+              <h1>Rain's Digital Café</h1>
+            </div>
+            <router-link to="#" class="button">GET TO KNOW ME!</router-link>
+          </div>
+        </div>
+      </header>
+  
+      <!-- Content Section -->
+      <div class="w3-sand w3-large">
+        <div class="w3-container" id="about">
+          <div class="w3-content" style="max-width: 1000px">
+            <h5 class="tag">ABOUT THE CAFE</h5>
+            <p>Welcome to my personal profile webpage!</p>
+            <p>
+              This is my very first step into the world of web development. I created
+              this page to explore and learn the basics of building a website while
+              expressing my style through a cozy café-inspired theme. It’s a small
+              but meaningful project that represents the beginning of my journey in
+              web design and development.
+            </p>
+          </div>
+        </div>
+  
+        <div class="w3-container" id="owner">
+          <div class="w3-content" style="max-width: 1000px">
+            <h5 class="tag">ABOUT THE OWNER</h5>
+            <div class="w3-panel w3-leftbar w3-light-beige">
+              <p><i>"The sun will rise and we will try again."</i></p>
+              <p>Chef, Coffeeist, and Owner: Rain Cel-Anne Sophia V. Presa</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        logoSrc:
+          "https://scontent.fmnl33-1.fna.fbcdn.net/v/t1.15752-9/474667107_1150394953328908_453092773794161443_n.png?_nc_cat=100&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeFtTV2Y3K0jKg0_2qV1Ho2xsF94E5Xqu6ewX3gTleq7pyQODnFyHMmvOYxLBKQlWA6rilKeyHQG2zaatLODHo2A&_nc_ohc=rpQ5djOAujEQ7kNvgHIFAZB&_nc_zt=23&_nc_ht=scontent.fmnl33-1.fna&oh=03_Q7cD1gF1ICi8zBFjshzJ24XNdz3oZT7SBQdxXjfFdQZv9Wfp3w&oe=67BFD3DF",
+      };
+    },
+  };
+  </script>
+
+  <style> </style>
