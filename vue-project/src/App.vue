91d8a8aaf44d93bb749067ad545dc0b9f883ab20
Rain Cel-Anne Sophia V. Presa <rvpresa@student.apc.edu.ph>
2025-02-10 11:42:10 +0800

    move layout

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,12 @@
 <template>
     <h1>Food</h1>
+    <comment-form/>
+    <comment/>
     <country/>
     <personal-profile/> <br>
     <food-item/> <br>
     <food-item2/> <br>
-    <comment-form/>
-    <comment/>
+
   </template>
   
   <script></script>
